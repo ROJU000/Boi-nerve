3fc84f3e9de8b6c113b9c19775db1aedacfacd05
Deroju <VENOM@macs-MacBook-Pro.local>
2020-11-20 13:35:26 +0100

    changed ajax placement

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
       
     <body class="nav-fixed">
         <nav class="topnav navbar navbar-expand shadow navbar-light bg-white" id="sidenavAccordion">
-            <a class="navbar-brand d-none d-sm-block" href="index-2.html">BOI Nerve</a><button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 mr-lg-2" id="sidebarToggle" href="#"><i data-feather="menu"></i></button>
+            <a class="navbar-brand d-none d-sm-block" href="{{ url() }}">BOI Nerve</a><button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 mr-lg-2" id="sidebarToggle" href="#"><i data-feather="menu"></i></button>
             <ul class="navbar-nav align-items-center ml-auto">               
                 <li class="nav-item dropdown no-caret mr-3 dropdown-user">
                     <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="https://source.unsplash.com/QAB-WJcbgJk/60x60"/></a>
